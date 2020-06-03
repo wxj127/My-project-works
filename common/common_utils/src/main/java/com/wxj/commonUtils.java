87eee2729fb18b778329f62f6897587bc85ebc2d
Administrator <1275144519@qq.com>
2020-06-03 17:39:07 +0800

    try

--- common/common_utils/src/main/java/com/wxj/commonUtils.java
+++
@@ -1,7 +0,0 @@
-package com.wxj;
-
-public interface commonUtils {
-    //定义状态码
-    public static Integer SUCCESS = 20000;
-    public static Integer ERROR   =  20001;
-}
--- service/service_msm/src/main/java/com/wxj/cmc/service/MsmService.java
+++
@@ -1,7 +0,0 @@
-package com.wxj.cmc.service;
-
-import java.util.Map;
-
-public interface MsmService {
-    boolean send(Map<String, Object> param, String phone);
-}
